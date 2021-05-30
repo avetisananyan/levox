ba117c9ab2c29aca7f96deb00769bf6c498e3df0
Gor Zakaryan <gor@zakaryan.com>
2021-05-30 23:01:26 +0400

    Worked on map

--- js/address.js
+++ js/address.js
@@ -1,16 +1,38 @@
+$('.tab-link').click( function() {
+    let tabID = $(this).attr('data-tab');
+    $(this).addClass('active').siblings().removeClass('active');
+    $('#tab-'+tabID).addClass('active').siblings().removeClass('active');
+});
+
+let arr = [];
+let obj = {};
+$(".addresses-map.active .address-item").each(function(index) {
+    obj = {
+        lat: $(this).attr("data-lat"),
+        lng: $(this).attr("data-lng"),
+        title: $(this).find(".item-title").text(),
+        description: $(this).find(".item-description span").text(),
+        date: $(this).find(".item-date span").text(),
+    };
+
+    arr.push(obj);
+});
+
 $(".address-items .address-item").click(function() {
-    $('.address-items .address-item').removeClass('active');
-    $(this).addClass('active');
+    if(window.matchMedia("(max-width: 768px)").matches) {
+        $('.address-items .address-item').removeClass('active');
+        $(this).addClass('active');
 
-    let itemTitle = $(this).find('.item-title span').text();
-    let itemDescription = $(this).find('.item-description span').text();
-    let itemDate = $(this).find('.item-date span').text();
+        let itemTitle = $(this).find('.item-title span').text();
+        let itemDescription = $(this).find('.item-description span').text();
+        let itemDate = $(this).find('.item-date span').text();
 
-    $('.map-addresses .map-addresses-title span').text(itemTitle);
-    $('.map-addresses .map-addresses-description').text(itemDescription);
-    $('.map-addresses .map-addresses-date').text(itemDate);
-
-    // myMap.setMarker();
+        $('.map-addresses .map-addresses-title span').text(itemTitle);
+        $('.map-addresses .map-addresses-description').text(itemDescription);
+        $('.map-addresses .map-addresses-date').text(itemDate);
+    } else {
+        myMap.setMarker();
+    }
 });
 
 if(window.matchMedia("(max-width: 768px)").matches) {
@@ -27,31 +49,14 @@ function initMap() {
     myMap.initialize();
 }
 
+let markers = [];
+
 let myMap = {
     map: null,
     markers: [],
     infowindow: null,
-    addressList: [{
-        name: 'LEVOX Краснодар 1',
-        lat: '51.480561',
-        lng: '-3.179239',
-    }, {
-        name: "LEVOX Краснодар 2",
-        lat: '51.479411',
-        lng: '-3.179182',
-    }, {
-        name: 'LEVOX Краснодар 3',
-        lat: '51.480742',
-        lng: '-3.181066',
-    }, {
-        name: 'LEVOX Краснодар 4',
-        lat: '51.479870',
-        lng: '-3.172483',
-    }, {
-        name: 'LEVOX Краснодар 5',
-        lat: '51.480740',
-        lng: '-3.181076',
-    }, ],
+    image: "assets/images/address-location.svg",
+    addressList: arr,
     initialize: function() {
         this.createMap();
         this.setMarkersAndInfoWindow();
@@ -73,35 +78,28 @@ let myMap = {
             let marker = new google.maps.Marker({
                 position: new google.maps.LatLng(address.lat, address.lng),
                 map: myMap.map,
-                title: address.name
+                title: address.name,
+                icon: this.image,
+                draggarble: false
             });
             this.markers.push(marker);
 
             google.maps.event.addListener(marker, 'click', function(marker, address) {
                 return function() {
-                    let content = $('#map-addresses').html();
-                    myMap.infowindow.setContent('<div id="addressFeatures">' + content + '</div>');
+                    myMap.infowindow.setContent('<div class="map-addresses"><div class="map-addresses-title"><span>' + address.title + '</span><a class="close"><img src="assets/icons/close.svg"></a></div><div class="map-addresses-images d-flex"><div class="map-addresses-img"><img src="assets/images/addresses-1.png"></div><div class="map-addresses-img"><img src="assets/images/addresses-2.png"></div><div class="map-addresses-img"><img src="assets/images/addresses-3.png"></div></div><div class="map-addresses-info-container"><div class="map-addresses-info"><label>Адрес:</label><span class="map-addresses-description">' + address.description + '</span></div><div class="map-addresses-info"><label>Режим роботы:</label><span class="map-addresses-date">' + address.date + '</span></div><div class="map-addresses-info"><label>Площадь магазина:</label><span>1 000 м2</span></div><div class="map-addresses-info"><label>Телефон:</label><span>+ 7 (918) 333-33-33</span></div></div></div>');
                     myMap.infowindow.open(myMap.map, marker);
+
+                    for (let j = 0; j < markers.length; j++) {
+                        markers[j].setIcon("assets/images/address-location.svg");
+                    }
+                    marker.setIcon("assets/images/address-location-3.svg");
                 };
             }(marker, address));
+
+            markers.push(marker);
         }
-
-        google.maps.event.addListener(myMap.infowindow, 'domready', function() {
-
-        });
     },
-    // setMarker: function() {
-    //     let address = this.addressList[1];
-    //     console.log(address);
-    //     let marker = new google.maps.Marker({
-    //         position: new google.maps.LatLng(address.lat, address.lng),
-    //         // map: myMap.map,
-    //         title: address.name
-    //     });
-    //
-    //     let content = $('#map-addresses').html();
-    //     console.log(myMap.map);
-    //     myMap.infowindow.setContent('<div id="addressFeatures">' + content + '</div>');
-    //     myMap.infowindow.open(marker);
-    // }
+    setMarker: function() {
+        google.maps.event.trigger(this.markers[1], 'click');
+    }
 };
